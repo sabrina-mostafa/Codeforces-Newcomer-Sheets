29d4ebf32dee250b60079fe410e2bd14e553e8f9
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2022-09-05 06:40:25 -0700

    Create D. Positions in array.c

---
+++ Sheet #3 (Arrays)/D. Positions in array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  int a[n+1],i;
+  for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+      if(a[i]<=10)
+      printf("A[%d] = %d\n",i,a[i]);
+    }
+}
