f48bddc4d8d5e379487159d4911d6a66e5ec8eef
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2022-09-05 03:42:03 -0700

    Create T. Shape2.c

---
+++ Sheet #2 (Loops)/T. Shape2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+  int n,i,j,k;
+  scanf ("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=i;j<n;j++)
+    printf(" ");
+    for(k=1;k<=i;k++)
+    printf ("*");
+    for(k=1;k<i;k++)
+    printf("*");
+    printf("\n");
+  }
+}
