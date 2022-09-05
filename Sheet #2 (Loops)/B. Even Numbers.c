20934dbbd09d8d3104d8228a86b5f30ab19603de
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2022-09-05 03:33:52 -0700

    Create B. Even Numbers.c

---
+++ Sheet #2 (Loops)/B. Even Numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int n,i;
+   scanf("%d",&n);
+   if(n==1)
+   printf("-1");
+   else
+   for(i=2;i<=n;i+=2)
+   {
+      printf("%d\n",i);
+   }
+   return 0;
+}
