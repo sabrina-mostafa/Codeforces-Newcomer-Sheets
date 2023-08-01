808742287515767b81cff88619812889a237c32d
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2023-08-01 03:41:12 -0700

    Create B. Print.cpp

---
+++ Sheet #5 (Functions)/B. Print.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void f(int x)
+{
+    cout<<1 ;
+  for(int i=2 ; i<=x; i++)
+  cout<< " "<<i ;
+}
+int main()
+{
+  int n ;
+  cin>>n ;
+  f(n) ;
+}
+ 
